93f3d5276309516d109988eca440113ad7ae312b
Anna <annafridman81@gmail.com>
2023-08-09 17:39:59 +0300

    README added, messages.py added to store chat-data, post_chat func added to flask-app.py

--- flask-app.py
+++ flask-app.py
@@ -1,43 +1,57 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 from datetime import datetime
+import os
 
-app = Flask(__name__)
+# Check if messages.py exists, if not, create it with an empty dictionary
+if not os.path.exists('messages.py'):
+    with open('messages.py', 'w') as f:
+        f.write('chat_data = {}\n')
 
-# Create a chat data dictionary to hold chat messages for different rooms.
-chat_data = {
+# Import chat_data from messages.py
+from messages import chat_data
 
-}
+app = Flask(__name__, static_url_path='')
 
 
-# Define the route for the root endpoint
-@app.route('/')
-@app.route('/<room>')# Serve the index.html file, regardless of whether a room is specified.
-def index():
-    # Serve the index.html file
+@app.route('/<room>')
+def room_redirect(room):
+    # This will redirect to the main chatroom view with the given room name
     return send_from_directory('.', 'index.html')
 
 
-# Define the GET endpoint to retrieve chat messages for a specific room.
 @app.route('/chat/<room>', methods=['GET'])
 def get_chat(room):
-    # Fetch chat messages for the specified room from the chat_data.
-    # If no messages exist for that room, return an empty list.
+    # Retrieve chat messages for the specified room
     chat = chat_data.get(room, [])
 
-    # Convert each chat entry to the specified string format.
-    formatted_chat = "\n".join(["[{date}] {username}: {message}".format(**entry) for entry in chat])
+    # Format the chat for display
+    formatted_chat = "\n".join(
+        ["[{date}] {username}: {message}".format(**entry) for entry in chat])
 
-    # Return the formatted chat messages as a response.
     return formatted_chat
 
 
+@app.route('/api/chat/<room>', methods=['POST'])
+def post_chat(room):
+    # Retrieve the username and message from the form data
+    username = request.form.get('username')
+    message = request.form.get('msg')
 
+    # Format the current time
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+    # Append the new message to chat_data
+    chat_data.setdefault(room, []).append({
+        "date": current_time,
+        "username": username,
+        "message": message
+    })
 
+    # Save the updated chat_data back to messages.py
+    with open('messages.py', 'w') as f:
+        f.write('chat_data = {}\n'.format(repr(chat_data)))
 
-
-
-
+    return jsonify(success=True)
 
 
 if __name__ == "__main__":
---
+++ messages.py
@@ -0,0 +1,1 @@
+chat_data = {'testroom': [{'date': '2023-08-09 17:35:36', 'username': 'anna', 'message': 'bla'}], 'VIProom': []}
